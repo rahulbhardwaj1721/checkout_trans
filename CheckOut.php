dfa285d28830332c2fe3d6fa8fc241fa0731908c
rahulbhardwaj1721 <rahulbhardwaj1721@gmail.com>
2021-09-24 00:37:11 +0530

    Update CheckOut.php

--- CheckOut.php
+++ CheckOut.php
@@ -62,3 +62,76 @@ $co->scanItem('b');
 $co->scanItem('c');
 $co->scanItem('d');
 echo $co->costItems();
+
+
+
+
+<?php
+
+function countArray($array1,$array2){
+     $result= (count($array1)>count($array2)) ? $array1 : $array2;
+     return $result;
+}
+$a=array(1,6,8,6);
+$b=array(1,1,5);
+$resultant=countArray($a,$b);
+$finalResult = array();
+$qoutient = 0;
+foreach($resultant as $key =>$value){
+    $valueSum=(@$a[count($a)-$key-1])+(@$b[count($b)-$key-1])+ $qoutient;
+    $qoutient = intval($valueSum/10);
+    $reminder = $valueSum%10;
+    $finalResult[] = $reminder;
+   
+}
+print_r(array_reverse($finalResult)) ;
+?>
+
+
+
+
+
+<?php
+
+
+function happyNo($input,$array){
+    $squareSum=0;
+    $input= "$input";
+    for($i = 0; $i < strlen($input);$i++){
+        $squareSum = $squareSum+pow($input[$i], 2);
+    }
+
+    if($squareSum ==1)
+        return true;
+
+    if(in_array($squareSum,$array))
+        return false;
+    $array[] =  $squareSum;  
+    return happyNo($squareSum,$array);
+}
+    
+
+$result = happyNo(13,[]);
+echo $result;
+
+?>
+
+
+loop of each digit and square sum
+<?php
+function square($n){
+    $squareSum= 0;
+    while($n){
+    $reminder = $n%10;
+    $squareSum +=$reminder*$reminder;
+    $n= intval($n/10);
+    
+    }
+    return $squareSum;
+}
+
+echo square(15);
+?>
+
+
+
